a0266d30d08b7c755fb58a3655221bf847e89d02
Abhijeet Gavkar <abhijeet@cateina.com>
2020-03-16 15:53:44 +0530

    commit by abhijeet for login

--- src/app/authentication/app-status/app-status.component.ts
+++ src/app/authentication/app-status/app-status.component.ts
@@ -360,25 +360,10 @@ export class AppStatusComponent implements OnInit, OnDestroy {
 
     ngOnInit() {
         $( "#initiateModal" ).trigger( "click" );	
-        var vTimeline = document.getElementById("horizontal-timeline");
-        var hTimeline = document.getElementById("vertical-timeline");
-        activation(vTimeline);
-        activation(hTimeline);
+        // $('.xterm-viewport').css("overflow","hidden")
+      
 
-function activation(timeline) {
-  var divs = timeline.getElementsByTagName("div");
-  for (var i = 0; i < divs.length; i++) {
-    if (
-      divs[i].classList.contains("circle") ||
-      divs[i].classList.contains("link")
-    ) {
-      divs[i].style.background = "#05A5D1";
-      if (divs[i].classList.contains("activated")) {
-        break;
-      }
-    }
-  }
-}
+
 
 
 
